127bd103bf11297020fad2fe37d630048d7d8790
Stefan Strableg <darkspark65@gmail.com>
2024-01-23 14:04:13 +0100

    Initial commit

---
+++ DynArray.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL_DynArray
+{
+    internal class DynArray : IHLContainer
+    {
+        private static float _GrowFactor = 1.5f;
+
+        private object[] _contents = new object[16];
+        private int _size = 0;
+        private int _current = 0;
+
+        private void _reallocate(int newSize)
+        {
+            object[] newArr = new object[newSize];
+            _contents.CopyTo(newArr, 0);
+            _contents = newArr;
+        }
+
+        private void _requireCapacity(int capacity)
+        {
+            if (_contents.Length >= capacity)
+                return;
+            int newCapacity = _contents.Length;
+
+            while (newCapacity < capacity)
+                newCapacity = (int)(_GrowFactor * (float)newCapacity);
+
+            _reallocate(newCapacity);
+        }
+
+        private int _find(object aObj, IComparer aCmp)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (aCmp.Compare(_contents[i], aObj) == 0)
+                    return i;
+            }
+            return -1;
+        }
+
+        private void _remove(int index)
+        {
+            if (index < 0 || index >= _size)
+                throw new IndexOutOfRangeException();
+            _size--;
+
+            for (int i = index; i < _size; i++)
+            {
+                _contents[i] = _contents[i + 1];
+            }
+
+            if (_current > index)
+                _current--;
+
+            if (_current == index && _current == _size)
+                _current--;
+
+        }
+
+        private void _insert(object obj, int index)
+        {
+            _size++;
+            _requireCapacity(_size);
+
+            if (_size == 1)
+                _current = 0;
+            else if (_current >= index)
+                _current++;
+
+            for (int i = _size - 1; i > index; i--)
+            {
+                _contents[i] = _contents[i - 1];
+            }
+
+
+            _contents[index] = obj;
+        }
+
+        public void AddHead(object aObj)
+        {
+            _insert(aObj, 0);
+        }
+
+        public void AddTail(object aObj)
+        {
+            _insert(aObj, _size);
+        }
+
+        public object At(int aPos)
+        {
+            if (_size <= aPos)
+                throw new IndexOutOfRangeException();
+            return _contents[aPos];
+        }
+
+        public int Count()
+        {
+            return _size;
+        }
+
+        public object Find(object aTestObject, IComparer aCmp)
+        {
+            int i = _find(aTestObject, aCmp);
+            if (i == -1)
+                return null;
+            return _contents[i];
+        }
+
+        public object First()
+        {
+            _current = 0;
+            if (_size == 0)
+                return null;
+            return _contents[0];
+        }
+
+        public void InsertAtPos(object aObj, int aPos)
+        {
+            _insert(aObj, aPos);
+        }
+
+        public void InsertSorted(object aObj, IComparer aCmp)
+        {
+            int i = 0;
+
+            while (i < _size - 1 && aCmp.Compare(aObj, _contents[i]) == 1)
+            {
+                i++;
+            }
+
+            if (_current >= i)
+                _current++;
+
+            _insert(aObj, i);
+        }
+
+        public object Next()
+        {
+            _current++;
+            if (_current >= _size)
+                return null;
+            return _contents[_current];
+        }
+
+        public void Print()
+        {
+            int cursorPosition = 0;
+            int cursorLength = 0;
+            if (_size == 0)
+            {
+                Console.WriteLine("Contents | ");
+                Console.WriteLine("Cursor   | ");
+                return;
+            }
+
+            Console.Write("Contents | ");
+            for (int i = 0; i < _size; i++)
+            {
+                if (i != 0)
+                    Console.Write(", ");
+                Console.Write(_contents[i]);
+                if (_current > i)
+                    cursorPosition += _contents[i].ToString().Length+2;
+                else if (_current == i)
+                    cursorLength = _contents[i].ToString().Length;
+            }
+
+            Console.WriteLine();
+
+            if (_current < _size)
+                Console.WriteLine("Cursor   | " + new String(' ', cursorPosition) + new String('^', cursorLength));
+            else
+                Console.WriteLine("No Cursor");
+        }
+
+        public object Remove(object aObj)
+        {
+            int i = _find(aObj, Comparer.Default);
+
+            if (i == -1)
+                return null;
+
+            _remove(i);
+            return aObj;
+
+        }
+
+        public object RemoveAt(int aIdx)
+        {
+            if (aIdx < 0 || aIdx >= _size)
+                throw new IndexOutOfRangeException();
+            object ret = _contents[aIdx];
+            _remove(aIdx);
+            return ret;
+        }
+
+        public object RemoveHead()
+        {
+            object ret = _contents[0];
+            _remove(0);
+            return ret;
+        }
+
+        public object RemoveTail()
+        {
+            object ret = _contents[_size-1];
+            _remove(_size-1);
+            return ret;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HL_DynArray;
+
+IHLContainer container = new DynArray();
+
+Random rng = new Random();
+
+// Add Head
+Console.WriteLine("Adding Head (0-9):\n");
+
+
+for (int i = 0; i < 10; i++)
+{
+    container.AddHead(i);
+    container.Print();
+}
+
+// Remove
+Console.WriteLine("\nRemoving by search (0-9):\n");
+
+for (int i = 0; i < 10; i++)
+{
+    container.Remove(i);
+    container.Print();
+}
+
+// Add Tail
+Console.WriteLine("\nAdding to tail (random between 0 - 1000):\n");
+
+for (int i = 0; i < 10; i++)
+{
+    container.AddTail(rng.Next() % 1000);
+    container.Print();
+}
+
+// Remove At
+Console.WriteLine("\nRemoving by index (9-0):\n");
+
+for (int i = 9; i >= 0; i--)
+{
+    container.RemoveAt(i);
+    container.Print();
+}
+
+// Insert At
+Console.WriteLine("\nInserting by index (0-9):\n");
+
+for (int i = 0; i < 10; i++)
+{
+    container.InsertAtPos(rng.Next() % 1000, i);
+    container.Print();
+}
+
+// Remove Head
+Console.WriteLine("\nRemoving head (10 times):\n");
+
+for (int i = 0; i < 10; i++)
+{
+    container.RemoveHead();
+    container.Print();
+}
+
+for (int i = 0; i < 10; i++)
+    container.InsertAtPos(i, i);
+
+// Remove Tail
+Console.WriteLine("\nRemoving tail (10 times):\n");
+
+for (int i = 0; i < 10; i++)
+{
+    container.RemoveTail();
+    container.Print();
+}
+
+for (int i = 0; i < 10; i++)
+    container.InsertAtPos(i, i);
+
+// Find
+Console.WriteLine("\nFinding in {0,1,2,3,4,5,6,7,8,9}:\n");
+
+Console.WriteLine("Find 3: " + container.Find(3, Comparer<int>.Default));
+Console.WriteLine("Find 8: " + container.Find(8, Comparer<int>.Default));
+Console.WriteLine("Find 12: " + container.Find(12, Comparer<int>.Default));
+
+// First
+Console.WriteLine("\nCalling First:\n");
+
+object o = container.First();
+
+Console.WriteLine("First: " + o);
+container.Print();
+
+// Next
+Console.WriteLine("\nCalling Next while it isnt null:\n");
+
+while ((o = container.Next()) != null)
+{
+    Console.WriteLine("Next: " + o);
+    container.Print();
+}
